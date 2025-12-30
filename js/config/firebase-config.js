8fe6f40875152008774629dbce9855a5137947d5
aderijke <aderijke@gmail.com>
2025-12-30 14:28:52 +0100

    Voeg firebase-config.js weer toe aan repository

---
+++ js/config/firebase-config.js
@@ -0,0 +1,23 @@
+// Firebase Configuration
+// 
+// INSTRUCTIES:
+// 1. Ga naar https://console.firebase.google.com/
+// 2. Maak een nieuw project aan of selecteer een bestaand project
+// 3. Ga naar Project Settings (⚙️) > General tab
+// 4. Scroll naar "Your apps" en klik op het web icoon (</>)
+// 5. Registreer je app (geef een naam op)
+// 6. Kopieer de Firebase configuratie object
+// 7. Vervang de onderstaande placeholder waarden met je eigen Firebase config
+
+// Firebase configuratie object (gebruikt met Firebase Compat SDK)
+const firebaseConfig = {
+  apiKey: "AIzaSyBxIFvQtxrxv_jYNeJxA8juBLIpIQq4X0w",
+  authDomain: "popsongchordbook.firebaseapp.com",
+  databaseURL: "https://popsongchordbook-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "popsongchordbook",
+  storageBucket: "popsongchordbook.firebasestorage.app",
+  messagingSenderId: "443671244182",
+  appId: "1:443671244182:web:98964f2fb2f174997041b7",
+  measurementId: "G-G9SJFCVE9Q"
+};
+
